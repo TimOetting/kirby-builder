0f2bc792bc79190e890b0ffb1bc10ec115f7d9b8
Dennis Markgraf <info@dennismarkgraf.com>
2016-09-27 19:51:07 +0200

    fixes add button labels for extended fieldsets field

--- template.php
+++ template.php
@@ -1,9 +1,9 @@
-<div class="structure<?php e($field->readonly(), ' structure-readonly') ?> builder<?php e($field->readonly(), ' builder-readonly') ?>" 
-  data-field="builder" 
-  data-api="<?php __($field->url('sort')) ?>" 
-  data-sortable="<?php e($field->readonly(), 'false', 'true') ?>" 
+<div class="structure<?php e($field->readonly(), ' structure-readonly') ?> builder<?php e($field->readonly(), ' builder-readonly') ?>"
+  data-field="builder"
+  data-api="<?php __($field->url('sort')) ?>"
+  data-sortable="<?php e($field->readonly(), 'false', 'true') ?>"
   data-style="<?php echo $field->style() ?>">
-  
+
   <div class="builder-label">
     <?php echo $field->headline() ?>
   </div>
@@ -20,12 +20,14 @@
   </div>
   <div class="structure-entry builder-entry builder-add-container hidden">
     <div class="builder-entry-quickform-container">
-      
+
     </div>
   </div>
   <div class="builder-add-buttons">
-    <?php foreach ($field->fieldsets as $fieldsetName => $fieldset): ?>
-      <a class="btn btn-rounded" data-quickform href="#" data-href="<?= purl($field->page, 'field/' . $field->name . '/builder/add?fieldset=' . $fieldsetName) ?>"><i class="icon icon-left fa fa-plus-circle"></i><?= $fieldset['label'] ?></a>
+    <?php foreach ($field->fieldsets as $fieldsetName => $fieldset):
+      $fieldset = $field->fieldset($fieldsetName);
+    ?>
+      <a class="btn btn-rounded" data-quickform href="#" data-href="<?= purl($field->page, 'field/' . $field->name . '/builder/add?fieldset=' . $fieldsetName) ?>"><i class="icon icon-left fa fa-plus-circle"></i><?= $fieldset->label ?></a>
     <?php endforeach ?>
   </div>
 
